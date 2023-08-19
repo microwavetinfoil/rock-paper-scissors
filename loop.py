e0b5c891119328ee51ba9ab02990157f213d1c31
microwavetinfoil <142690235+microwavetinfoil@users.noreply.github.com>
2023-08-19 15:11:20 -0400

    loop.py

--- loop.py
+++ loop.py
@@ -1,1 +1,15 @@
+#simple.py
+import random
 
+CHOICES = ["rock", "paper", "scissors"]
+
+print("make your throw ")
+
+user_choice = input (  type rock, paper, or scissors)
+
+if user_choice in CHOICES
+  computer_choice = random.choice(CHOICES)
+  print(
+    f"\n You threw '{user_choice}' ,the computer threw '{computer_choice}")
+else:
+  print(f"\n you typed '(user_choice)' which wasn't a valid choice")  
